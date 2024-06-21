4ba7ec843a696f9320a633620a1d6d56e0e8e467
u_m24yl <ASGorelkin@alfabank.ru>
2024-06-21 22:45:30 +0300

    Initial Commit

---
+++ TodoYandex/TodoYandexApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoYandexApp.swift
+//  TodoYandex
+//
+//  Created by Александр Горелкин on 21.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoYandexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
